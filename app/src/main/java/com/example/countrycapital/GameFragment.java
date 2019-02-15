2d23ac4e1dbc3f05f0daa6057ead736ee112ab3a
antodup <anthony_dupre@live.fr>
2019-02-15 18:19:16 +0100

    second

--- app/src/main/java/com/example/countrycapital/GameFragment.java
+++ app/src/main/java/com/example/countrycapital/GameFragment.java
@@ -7,14 +7,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class GameFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
+    TextView name_input;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        String namePLayer = getArguments().getString("namePlayer");
+        View view = inflater.inflate(R.layout.fragment_game, container, false);
+        name_input = (TextView) view.findViewById(R.id.name_input);
+        name_input.setText(namePLayer);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        return view;
+
+
+
     }
 
 }
